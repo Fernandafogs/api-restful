098228180f7353b460736c87be58828ac9536057
Fernanda Fogaca <fernanda.ramos.br@gmail.com>
2023-07-20 15:53:41 +0100

    adicionando metodo get

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,10 +1,29 @@
 const ProductsModel = require ('../models/products')
 
 async function get(req, res) {
-    const products = await ProductsModel.find()
+    const {id} = req.params
+
+    let obj = {}
+
+    if (id){
+        obj._id = id
+    }
+    const products = await ProductsModel.find(obj)
 
     res.send(products)
-}
+    }
+
+    /* ou fazer um if ternario:
+    async function get(req, res) {
+    const {id} = req.params
+
+    const obj = id ? { _id: id } : null
+
+    const products = await ProductsModel. find (obj)
+
+    res.send (products)
+    }
+*/
 
 module.exports = {
     get,
